83c33966c1abf061de6579dfc416cd64f68eac2e
Aurelio Sosa <ezequielaureliososauy@gmail.com>
2023-10-04 19:55:16 -0300

    se agrego al menu el carrito y se hizo responsibe disain, tambien se programo para que se cierren automaticamente los otros menus cuando se abre uno

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
+const menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const shopCar = document.querySelector(".navbar-shopping-cart");
+const shopCarMenu = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menu.addEventListener("click", toggleMenu)
+shopCar.addEventListener("click", toggleShopCar)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
-    
+    mobileMenu.classList.add("inactive")
+    shopCarMenu.classList.add("inactive")
 }
-
-const menu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-
-menu.addEventListener("click", toggleMenu)
 
 function toggleMenu() {
     mobileMenu.classList.toggle("inactive");
-    
+    shopCarMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+}
+
+function toggleShopCar() {
+    shopCarMenu.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
 }
