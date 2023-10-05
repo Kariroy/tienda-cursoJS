7baef7bb448d0af10decd1cfab9f3c3cc256df38
Aurelio Sosa <ezequielaureliososauy@gmail.com>
2023-10-05 00:07:02 -0300

    Ajustes en el css se agregaron los detalles de producto

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const shopCar = document.querySelector(".navbar-shopping-cart");
-const shopCarMenu = document.querySelector(".product-detail");
+const shopCarMenu = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container")
 const productList = [];
 
@@ -46,6 +46,8 @@ function insertar(arreglo) {
     for (let i = 0; i < arreglo.length; i++) {
         let div = document.createElement("div")
         div.classList.add("product-card")
+        let clase = arreglo[i].name
+        div.classList.add(clase)
         div.innerHTML = `
         <img src=${arreglo[i].image} alt="">
         <div class="product-info">
@@ -62,11 +64,15 @@ function insertar(arreglo) {
     }
 }
 
+
+
+
+
 newProduct("bike",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-newProduct("bike",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-newProduct("bike",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-newProduct("bike",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-newProduct("bike",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-newProduct("bike",200,"https://motos-b60.kxcdn.com/sites/default/files/yamaha-r6-race-2022.jpg")
+newProduct("cuadro",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+newProduct("cuatrio",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+newProduct("skate",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+newProduct("pelota",200,"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+newProduct("moto",200,"https://motos-b60.kxcdn.com/sites/default/files/yamaha-r6-race-2022.jpg")
 
 insertar(productList)
